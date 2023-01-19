f18ebdc1fd0ea8e97d1a7aa5552b9fcedf5edc72
Tony <debuggert@nahab.info>
2023-01-19 15:27:47 +0300

    initial commit

---
+++ src/components/app/App.js
@@ -0,0 +1,14 @@
+import CardCat from '../cardCat/CardCat';
+import './app.css';
+
+function App() {
+  return (
+    <div className="site_wrap">
+      <main className="main">
+        <CardCat />
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/cardCat/CardCat.js
@@ -0,0 +1,14 @@
+import React, { PureComponent } from 'react'
+
+import './cardCat.css';
+
+const CardCat = (props) => {
+    const {allEmployess, incEmploess} = props;
+    return (
+        <div className="card">
+            This is card
+        </div>
+    );
+}
+
+export default CardCat;
